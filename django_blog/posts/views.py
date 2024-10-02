ef4abf08cddc0d6db8498ed9c208dbcce9a5e640
kimi.casamina <kimi.casamina@gmail.com>
2024-10-02 23:44:07 +0800

    added .venv to gitignore

---
+++ django_blog/posts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+# Create your views here.
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/posts_list.html', {'posts': posts})
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug);
+    return render(request, 'posts/post_page.html', {'post': post})
